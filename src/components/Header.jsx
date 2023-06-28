676deeb5d4f3e0ada79071edac1b0c1dcee47d87
liamonk <61438758+liamonk@users.noreply.github.com>
2023-06-28 14:45:15 +1200

    finished the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <div className="header--div">
+          <img className="header--img" src="/troll.png" />
+          <h2>Meme Generator</h2>
+        </div>
+        <h4>React Course - Project 3</h4>
+      </header>
+    </>
+  );
+}
