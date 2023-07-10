3882dc045d6146c70398fa254d37a9b9e7b5517a
liam <liamokeegan@gmail.com>
2023-07-10 16:03:41 +1200

    making the app interact with data file

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,28 @@
 import React from "react";
+import data from "../memesData.js";
 
 export default function () {
+  function getRandomMemeImageUrl() {
+    return console.log(
+      data.data.memes[Math.floor(Math.random() * data.data.memes.length)].url
+    );
+  }
   return (
     <>
       <div className="inputs">
-        <input className="inputs--input" placeholder="Top Text"></input>
-        <input className="inputs--input" placeholder="Bottom Text"></input>
+        <input
+          type="text"
+          className="inputs--input"
+          placeholder="Top Text"
+        ></input>
+        <input
+          type="text"
+          className="inputs--input"
+          placeholder="Bottom Text"
+        ></input>
       </div>
       <div className="button--container">
-        <button>Get a new meme image!</button>
+        <button onClick={getRandomMemeImageUrl}>Get a new meme image!</button>
       </div>
     </>
   );
