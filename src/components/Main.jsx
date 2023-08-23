4d34d38e80ff2aa7150c65853554079bfc20bbe5
liamonk <61438758+liamonk@users.noreply.github.com>
2023-08-24 08:11:18 +1200

    finished product (still a few bugs)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,29 +1,87 @@
 import React from "react";
-import data from "../memesData.js";
 
 export default function () {
-  function getRandomMemeImageUrl() {
-    return console.log(
-      data.data.memes[Math.floor(Math.random() * data.data.memes.length)].url
-    );
+  const [allMemeImages, setAllMemeImages] = React.useState([]);
+  const [memeData, setMemeData] = React.useState({
+    topText: "Top text",
+    bottomText: "Bottom text",
+    randomImageUrl: "",
+  });
+
+  React.useEffect(() => {
+    setMemeData(JSON.parse(localStorage.getItem("memes")));
+  }, []);
+
+  React.useEffect(() => {
+    localStorage.setItem("memes", JSON.stringify(memeData));
+  }, [memeData]);
+
+  React.useEffect(function () {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemeImages(data.data.memes));
+  }, []);
+
+  function getRandomMemeImage() {
+    console.log(allMemeImages);
+    const memesArray = allMemeImages;
+    const randomMemeIndex = Math.floor(Math.random() * allMemeImages.length);
+    const url = allMemeImages[randomMemeIndex].url;
+    setMemeData((prevMeme) => ({ ...prevMeme, randomImageUrl: url }));
   }
+
+  function resetForm() {
+    setMemeData({
+      topText: "",
+      bottomText: "",
+      randomImageUrl: "",
+    });
+  }
+
+  function handleTextChange(event) {
+    setMemeData((prevMeme) => {
+      return { ...prevMeme, [event.target.name]: event.target.value };
+    });
+  }
+
+  console.log(allMemeImages);
+
   return (
     <>
+      <div className="input-headings">
+        <h4>Top text</h4>
+        <h4>Bottom Text</h4>
+      </div>
       <div className="inputs">
         <input
           type="text"
           className="inputs--input"
-          placeholder="Top Text"
+          placeholder={memeData.topText}
+          onChange={handleTextChange}
+          name="topText"
+          value={memeData.topText}
         ></input>
+
         <input
           type="text"
           className="inputs--input"
-          placeholder="Bottom Text"
+          placeholder={memeData.bottomText}
+          onChange={handleTextChange}
+          name="bottomText"
+          value={memeData.bottomText}
         ></input>
       </div>
       <div className="button--container">
-        <button onClick={getRandomMemeImageUrl}>Get a new meme image!</button>
+        <button onClick={getRandomMemeImage}>Get a new meme image!</button>
       </div>
+      <div className="memeContainer">
+        <img className="memeImage" src={memeData.randomImageUrl} />
+        <h2 className="topText">{memeData.topText}</h2>
+        <h2 className="bottomText">{memeData.bottomText}</h2>
+      </div>
+      <button className="resetButton" onClick={resetForm}>
+        Reset the form!
+      </button>
     </>
   );
 }
